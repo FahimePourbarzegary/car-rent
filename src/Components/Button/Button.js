4a2e08454ad4f83fbeabb47c2288cab9ee00a1e7
fahime <fahimeh757@gmail.com>
2022-10-27 14:11:37 +0330

    style:Button component

---
+++ src/Components/Button/Button.js
@@ -0,0 +1,14 @@
+const Button = () => {
+  return (
+    <>
+      <button
+        type="submit"
+        className=" bg-blue-700 w-24 h-9 rounded text-white font-semibold text-base hover:bg-blue-500 hover:w-28 md:w-28 md:h-11"
+      >
+        حالا اجاره کن
+      </button>
+    </>
+  );
+};
+
+export default Button;
