3e8b4aaffba3446c4528602900a810cdcf86ad95
fahime <fahimeh757@gmail.com>
2022-10-31 22:05:27 +0330

    add Toastify

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
-
+import { ToastContainer} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage/>}/>
-    </Routes>
+    <>
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </>
   );
 }
 
